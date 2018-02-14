64ed22e0f4d866961ff3de2926d1d0ca5d60efd3
imahdi2000 <imahdi@stuy.edu>
2018-02-14 15:19:26 -0500

    started

---
+++ mongo.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+#sudo pip install pymongo
+#def borough():
+
+
+
+def run():
+    connection = MongoClient("homer.stuy.edu")
+    db = connection.test
+
+
+
+
+run()
